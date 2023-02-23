8b5cc8fcf54228cd11fbf641b8ad5e08ea713396
Shravan-scientist <105781402+Shravan-scientist@users.noreply.github.com>
2023-02-23 16:37:34 +0530

    Delete mle_training_module3 directory

--- mle_training_module3/notebooks/main.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-# from ../src import ingest_data, score, train
-
-subprocess.run(["python", "../src/ingest_data.py"])
-subprocess.run(["python", "../src/train.py"])
-subprocess.run(["python", "../src/score.py"])
